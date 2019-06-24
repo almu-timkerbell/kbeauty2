83dd29ae5fd52aa284630ee0c318364bc4cdd7e8
Almu <almu_atleticahotmail.com>
2019-06-24 17:20:07 +0100

    imagenes, javascript

---
+++ kbeauty.js
@@ -0,0 +1,15 @@
+var d = document.getElementById("titulo");
+d.addEventListener("mouseover", desplegar);
+
+d.addEventListener("mouseout", originalTitle);
+
+function desplegar()
+{
+d.innerHTML = "Welcome to KOB";
+
+}
+
+function originalTitle()
+{
+d.innerHTML = "Korean Original Beauty";
+}
