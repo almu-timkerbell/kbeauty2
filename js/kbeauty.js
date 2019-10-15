626588d5c094dd3a970bfd0230024e7c9278099f
Almu <almu_atleticahotmail.com>
2019-10-15 18:42:16 +0100

    poniendo img

--- js/kbeauty.js
+++ js/kbeauty.js
@@ -26,14 +26,5 @@ searchText.addEventListener('click', (event) =>{
       
 });
 
-function waithide()
-{
-  var obj = document.getElementById("thisone");
-  obj.style.opacity = '0';
-  window.setTimeout(
-    function removethis()
-    {
-      obj.style.display='none';
-    }, 300);
-}
+
 
